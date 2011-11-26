6116e2d14b1e5c8b1730e675af8389f7e0ba5769
Constantin Titarenko <constantin.titarenko@gmail.com>
2011-11-27 00:14:59 +0200

    Added WebUi powered by MVC3.

---
+++ Market.Cqrsnes.WebUi/Controllers/ArticleController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Market.Cqrsnes.WebUi.Controllers
+{
+    public class ArticleController : Controller
+    {
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
