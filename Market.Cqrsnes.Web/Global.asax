35dd7ba24385f5a348f5bbe4acde44276d0380a1
Constantin Titarenko <constantin.titarenko@gmail.com>
2011-11-27 01:31:26 +0200

    Removed old project (Web).

--- Market.Cqrsnes.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Market.Cqrsnes.Web.MvcApplication" Language="C#" %>
--- Market.Cqrsnes.Web/Models/CreateArticleViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Market.Cqrsnes.Web.Models
-{
-    public class CreateArticleViewModel
-    {
-        public string Name { get; set; }
-    }
-}
