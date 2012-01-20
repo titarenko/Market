4d97f318fa4f85885d795f19ceee26fa25da81d1
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-01-21 01:42:15 +0200

    Added form/action for store creation.

--- Market.Cqrsnes.WebUi/Controllers/StoreController.cs
+++ Market.Cqrsnes.WebUi/Controllers/StoreController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Web.Mvc;
 using Cqrsnes.Infrastructure;
+using Market.Cqrsnes.Domain.Commands;
 using Market.Cqrsnes.Projection;
 
 namespace Market.Cqrsnes.WebUi.Controllers
@@ -8,10 +9,12 @@ namespace Market.Cqrsnes.WebUi.Controllers
     public class StoreController : Controller
     {
         private readonly IRepository repository;
+        private readonly IBus bus;
 
-        public StoreController(IRepository repository)
+        public StoreController(IRepository repository, IBus bus)
         {
             this.repository = repository;
+            this.bus = bus;
         }
 
         public ActionResult List()
@@ -22,6 +25,18 @@ namespace Market.Cqrsnes.WebUi.Controllers
         public ActionResult Offers(Guid id)
         {
             return View(repository.GetSingle<StoreOffers>());
+        }
+
+        [HttpPost]
+        public ActionResult Create(string name)
+        {
+            bus.Send(new CreateStore
+                {
+                    Id = Guid.NewGuid(),
+                    Name = name
+                });
+
+            return RedirectToAction("List");
         }
     }
 }
