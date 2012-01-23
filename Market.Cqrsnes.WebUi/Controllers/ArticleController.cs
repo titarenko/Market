1adc63588bef936b59cd785b3f79c80f77eebffa
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-01-23 20:09:19 +0200

    Cleaned up article controller. Added documentation.

--- Market.Cqrsnes.WebUi/Controllers/ArticleController.cs
+++ Market.Cqrsnes.WebUi/Controllers/ArticleController.cs
@@ -1,80 +1,54 @@
 ﻿using System;
 using System.Web.Mvc;
 using Cqrsnes.Infrastructure;
-using Market.Cqrsnes.Domain;
 using Market.Cqrsnes.Domain.Commands;
-using Market.Cqrsnes.Domain.Events;
-using Market.Cqrsnes.Domain.Test;
 using Market.Cqrsnes.Projection;
-using Market.Cqrsnes.Projection.Test;
-using Market.Cqrsnes.WebUi.Models;
 
 namespace Market.Cqrsnes.WebUi.Controllers
 {
-    [HandleError]
+    /// <summary>
+    /// Hosts article-related actions.
+    /// </summary>
     public class ArticleController : Controller
     {
         private readonly IBus bus;
         private readonly IRepository repository;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArticleController"/> class.
+        /// </summary>
+        /// <param name="bus">
+        /// The bus.
+        /// </param>
+        /// <param name="repository">
+        /// The repository.
+        /// </param>
         public ArticleController(IBus bus, IRepository repository)
         {
             this.bus = bus;
             this.repository = repository;
         }
 
+        /// <summary>
+        /// Lists articles.
+        /// </summary>
+        /// <returns>
+        /// List of articles.
+        /// </returns>
         public ActionResult List()
         {
             return View(repository.GetAll<Article>());
         }
 
-        [HttpPost]
-        public ActionResult ChangeCount(ChangeCountViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                bus.Send(model.IsDelivery
-                             ? (Command)new SupplyArticle
-                             {
-                                 OfferId = model.Id,
-                                 Count = model.Count
-                             }
-                             : (Command)new BuyArticle
-                             {
-                                 OfferId = model.Id,
-                                 Count = model.Count
-                             });
-
-            }
-
-            return RedirectToAction("List");
-        }
-
-        [HttpPost]
-        public ActionResult CreateArticle(CreateArticleViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                bus.Send(new CreateArticle
-                {
-                    Id = Guid.NewGuid(),
-                    Name = model.Name
-                });
-            }
-
-            return RedirectToAction("List");
-        }
-
-        public ActionResult DomainTest()
-        {
-            return View("Test", new /*ArticleSpecifications().ExecuteAll()*/object());
-        }
-
-        public ActionResult ProjectionTest()
-        {
-            return View("Test", new ArticleViewModelManagerSpecifications().ExecuteAll());
-        }
-
+        /// <summary>
+        /// Creates new article.
+        /// </summary>
+        /// <param name="name">
+        /// The name.
+        /// </param>
+        /// <returns>
+        /// Redirect to list of articles.
+        /// </returns>
         [HttpPost]
         public ActionResult Create(string name)
         {
