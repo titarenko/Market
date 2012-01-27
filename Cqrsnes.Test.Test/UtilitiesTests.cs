0a126e12cc119cc3009aab8e4adcefc69878d946
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-01-27 23:50:18 +0200

    Added few tests for Cqrsnes.Test.Utility helper class.

---
+++ Cqrsnes.Test.Test/UtilitiesTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Cqrsnes.Test.Test
+{
+    [TestFixture]
+    public class UtilitiesTests
+    {
+        [Test]
+        [TestCase("CanPrettifyThis", "can prettify this")]
+        [TestCase("Can_Prettify_This", "can prettify this")]
+        [TestCase("CanPrettify_This", "can prettify this")]
+        [TestCase("canPrettify_this", "can prettify this")]
+        public void CanPrettify(string given, string expected)
+        {
+            Assert.AreEqual(expected, Utilities.Prettify(expected));
+        }
+    }
+}
